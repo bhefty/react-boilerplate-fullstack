5e568c33e33f523ac30d479e950516d5017a8606
Bill Hefty <billhefty@gmail.com>
2017-02-19 12:57:51 -0600

    Add dummyData template

---
+++ server/util/dummyData.js
@@ -0,0 +1,32 @@
+// Import model here
+
+export default function() {
+  // MODEL_HERE.count().exec((err, count) => {
+  //   if (count > 0) {
+  //     return
+  //   }
+  //
+  //   const doc1 = new MODEL_HERE({
+  //     name: 'John Smith',
+  //     age: 27
+  //   })
+  //
+  //   const doc2 = new MODEL_HERE({
+  //     name: 'Jane Doe',
+  //     age: 25
+  //   })
+  //
+  //   const doc2 = new MODEL_HERE({
+  //     name: 'Ben Jamin',
+  //     age: 30
+  //   })
+  //
+  //   MODEL_HERE.create([doc1, doc2, doc3], (error) => {
+  //     if (!error) {
+  //       console.log('Dummy data inserted')
+  //     } else {
+  //       console.log('Error', error)
+  //     }
+  //   })
+  // })
+}
